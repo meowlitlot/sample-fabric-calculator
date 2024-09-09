7264cf5883ac7f064603b9338cf5a1b2cc9476f5
Litlot <61440228+meowlitlot@users.noreply.github.com>
2024-09-09 23:07:01 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,20 +6,17 @@ class YarnDetail:
     cal_weight: float = 0
 
     def cal_line_length(self):
-        try:
             return f"{self.raw_length}cm/{self.raw_count}针"
-        except ZeroDivisionError:
-            return "0针"
 
     def cal_staple(self):
         try:
-            return f"{self.raw_length * self.cal_count * 0.00591903719 / self.cal_weight:.2f}S"
+            return f"{self.cal_length * self.cal_count * 0.00591903719 / self.cal_weight:.2f}S"
         except ZeroDivisionError:
             return "0S"
 
     def cal_denier(self):
         try:
-            return f"{5314.5 / (self.raw_length * self.cal_count * 0.00591903719 / self.cal_weight):.2f}D"
+            return f"{5314.5 / (self.cal_length * self.cal_count * 0.00591903719 / self.cal_weight):.2f}D"
         except ZeroDivisionError:
             return "0D"
 
